f9e3f21a33e9fb2cab85458fdc3e62b41d20ad9c
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-12 14:39:21 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,11 +18,14 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
+   
+}
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("org.opencv:opencv:4.10.0")
+
 }
