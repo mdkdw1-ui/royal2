48111b98a5473459c1ba2fd511c00b5d57b86519
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-13 00:45:20 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,13 +7,10 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    // 🟢 모든 모듈의 저장소 설정을 여기서 강제 통합 (중복 방지)
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // 🟢 JitPack 저장소 추가
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
