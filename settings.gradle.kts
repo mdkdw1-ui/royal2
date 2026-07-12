13628a12cc8626fc4399f8833770e5885d01358e
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-12 22:34:33 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,16 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    // 🟢 개별 프로젝트 레벨에서 의존성 저장소를 자유롭고 확실하게 제어하도록 허용합니다.
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    // 🟢 모든 모듈의 저장소를 전역에서 중앙 제어하도록 강제합니다.
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // 🟢 JitPack 저장소를 전역에 안전하게 등록하여 401/404 인증 우회를 해결합니다.
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 
 rootProject.name = "royal2"
