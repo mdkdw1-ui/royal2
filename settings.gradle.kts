0cff373483e832ecb1306f130b9835371a328741
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-18 00:42:45 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,17 +10,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
-        
-        // ✨ 다른 저장소 인증 정보(GitHub Packages 토큰 등)가 간섭하여 발생하는 401 에러를 막기 위해
-        // JitPack 요청 시 빈(Empty) 인증 정보를 강제로 사용하도록 설정합니다.
-        maven { 
-            url = uri("https://jitpack.io") 
-            credentials {
-                username = ""
-                password = ""
-            }
-        }
+        mavenCentral() // 🧹 속 썩이던 JitPack 관련 코드는 깔끔하게 삭제!
     }
 }
 
