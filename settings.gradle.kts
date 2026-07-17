a8bf1eb54b52293353c785a843027def1f1c9d3c
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-18 00:59:05 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral() // 🧹 속 썩이던 JitPack 관련 코드는 깔끔하게 삭제!
+        mavenCentral() // 👍 깨끗하게 여기만 남겨둡니다.
     }
 }
 
